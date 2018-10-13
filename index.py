f67ac77acee9c88b5a31851560485dcf8497e3dc
Daniel Yoo <yoo.d93@gmail.com>
2018-10-13 00:44:24 -0400

    initial commit with BFS, DFS implemented

---
+++ index.py
@@ -0,0 +1,26 @@
+from puzzle import Puzzle
+
+# read input
+size_x = 4
+size_y = 3
+# input = [1,2,0,4,5,6,7,3,9,10,11,8]
+input = [0,6,11,7,3,2,10,8,1,5,4,9]
+
+# faster input
+# size_x = 3
+# size_y = 3
+# input = [1,2,3,4,0,6,7,8,5]
+
+#fasstest input
+# size_x = 3
+# size_y = 2
+# input = [1,0,2,4,5,3]
+
+# validate input
+
+# init the puzle
+puzzle = Puzzle(size_x, size_y, input)
+
+# BFS with heuristic A
+puzzle.BFS(puzzle.heuristicA)
+puzzle.generateSolution("BFS_A")
---
+++ puzzle.py
@@ -0,0 +1,162 @@
+import heapq
+
+class Puzzle:
+    current_state = []
+    open = []
+    closed = {}
+    solution_path = {}
+    size_x = 0
+    size_y = 0
+
+    def __init__(self, size_x, size_y, state):
+        self.size_x = size_x
+        self.size_y = size_y
+        self.current_state = state
+
+        if self.isSolvable(state) is False:
+            print("This board has no solutions")
+            return False
+
+    def DFS(self):
+        print("Running depth first search . . .")
+        self.open.append(self.current_state)
+
+        while self.open:
+            self.current_state = self.open.pop()
+            key = tuple(self.current_state)
+
+            # put into close state
+            self.closed[key] = 1
+
+            if self.isFinalState():
+                print("FOUND FINAL STATE!")
+                # self.generateSolution()
+                return True
+
+            # else, push all its available next states into the open list
+            self.pushNextStates()
+
+        return False
+
+    def BFS(self, heuristic):
+        print("Running best first search . . .")
+        heapq.heappush(self.open, (self.heuristicA(self.current_state), self.current_state))
+
+        while self.open:
+            # pop current state
+            self.current_state = heapq.heappop(self.open)[1]
+            
+            # put into closed state
+            key = tuple(self.current_state)
+            self.closed[key] = 1
+
+            # check if final state
+            if self.isFinalState():
+                print("FOUND FINAL STATE!")
+                # self.generateSolution()
+                return True
+            else:
+                # push next states 
+                self.pushNextStates(heuristic)
+            # repeat
+
+
+        return False
+
+    def pushNextStates(self, heuristic=None):
+        start_x = -1
+        end_x = 1
+        start_y = -1
+        end_y = 1
+        start_index = self.current_state.index(0)
+
+        # if first row
+        if start_index <= self.size_x:
+            end_y = 0
+
+        # if last row
+        if start_index >= (self.size_x * (self.size_y - 1)):
+            start_y = 0
+
+        # if first column
+        if start_index % self.size_x == 0:
+            start_x = 0
+
+        # if last column
+        if start_index % self.size_x == self.size_x - 1:
+            end_x = 0
+    
+
+        # TODO:: how to order these in a clockwise fashion
+        for x in range(start_x, end_x + 1):
+            for y in range(start_y, end_y + 1):
+                if x != 0 or y != 0:
+                    # create the new state with the move done
+                    new_state = self.current_state.copy()
+                    swap_index = start_index + x + -y * self.size_x 
+                    self.swap(start_index, swap_index, new_state)
+
+                    # put into open list and also into solution path
+                    # check if this state exists in closed
+                    if tuple(new_state) not in self.closed:
+                        # if heuristic is passed, you need to push a tuple of (h, state)
+                        if heuristic is not None:
+                            heapq.heappush(self.open, (heuristic(new_state), new_state))
+                        else:
+                            self.open.append(new_state)
+                        
+                        # push tuple of state, parent_state to trace back solution path
+                        self.solution_path[tuple(new_state)] = self.current_state
+
+
+    def isFinalState(self):
+        previous = -1
+
+        for i in range(len(self.current_state) - 1):
+            if previous < self.current_state[i]:
+                previous = self.current_state[i]
+            else:
+                return False
+        
+        return True
+
+    def swap(self, i, j, state):
+        temp = state[i]
+        state[i] = state[j]
+        state[j] = temp
+
+    def generateSolution(self, filename):
+        base_char = 'a'
+        stack = []
+        file = open(filename + ".txt", "w")
+        
+        key = tuple(self.current_state)
+        stack.append(key)
+
+        # push into text file, also reverse the order. (push into a stack then pop them out for printing)
+        while key in self.solution_path:
+            key = tuple(self.solution_path[key])
+            stack.append(key)
+
+        file.write("0 " + str(stack.pop()) + "\n")
+
+        while stack:
+            state = stack.pop()
+            file.write(chr(ord(base_char) + state.index(0)) + " " + str(state) + "\n")
+
+
+    # TODO:: check if board is solvable
+    def isSolvable(self, state):
+        return True
+
+    # hamming distance
+    def heuristicA(self, state):
+        sum = 0
+
+        for i in range(len(state)):
+            # compare the current index with the right index
+            if state[i] != 0:
+                if i != (state[i] - 1):
+                    sum += 1
+
+        return sum
